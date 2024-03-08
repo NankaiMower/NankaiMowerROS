7ce95f572f3bf456230dc7215cd5c47b6839704c
JunyaoHu <hujunyao@mail.nankai.edu.cn>
2024-03-08 21:39:58 +0800

    [update] 初步构建ros空间，输出测试

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/mower/mower/NankaiMowerROS/src
+# Build directory: /home/mower/mower/NankaiMowerROS/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("nankai_mower")
---
+++ build/nankai_mower/catkin_generated/stamps/nankai_mower/hello.py.stamp
@@ -0,0 +1,5 @@
+import rospy
+
+if __name__ == "__main__":
+    rospy.init_node("Hello")
+    rospy.loginfo("Hello World!!!!")
