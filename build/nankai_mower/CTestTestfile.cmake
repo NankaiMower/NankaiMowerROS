c088ffa06f7abeffd989ab7820a38e47f6801398
JunyaoHu <hujunyao@mail.nankai.edu.cn>
2024-03-14 23:44:23 +0800

    [delete] build and devel

--- build/nankai_mower/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mower/project/NankaiMowerROS/src/nankai_mower
-# Build directory: /home/mower/project/NankaiMowerROS/build/nankai_mower
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
